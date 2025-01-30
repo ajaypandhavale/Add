8902d532bbc46ea98fb7a2f3caf2592869dc717a
Ajay Pandhavale <aj40135821@wipro.com>
2025-01-30 16:07:28 +0530

    pushing source code

---
+++ src/Add.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Add {
+    public static void main (String[] args){
+        Scanner sc=new Scanner(System.in);
+
+        int a;
+        int b;
+
+        System.out.println("enter number to perform addition ");
+        a=sc.nextInt();
+        b=sc.nextInt();
+
+        int sum=a+b;
+
+        System.out.println("sum of the given number"+sum);
+
+        System.out.println("pushing code using intellij");
+
+
+    }
+}
